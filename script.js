f2f773753fc58f0ce8d7b21e6c502e0aeb65d3f8
yap19052001 <yap19052001@gmail.com>
2024-06-19 18:32:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// document.getElementById('fullscreenButton').addEventListener('click', function () {
-//     if (document.documentElement.requestFullscreen) {
-//         document.documentElement.requestFullscreen();
-//     } else if (document.documentElement.mozRequestFullScreen) { // Firefox
-//         document.documentElement.mozRequestFullScreen();
-//     } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
-//         document.documentElement.webkitRequestFullscreen();
-//     } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
-//         document.documentElement.msRequestFullscreen();
-//     }
-// });
+document.getElementById('fullscreenButton').addEventListener('click', function () {
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+        document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+        document.documentElement.msRequestFullscreen();
+    }
+});
 
 function usersLocationUpdated() { }
 // function heatMapUpdated() { }
