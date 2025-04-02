88dd7b08491e90f464e52803861f89739e9b6889
yap19052001 <yap19052001@gmail.com>
2025-04-02 11:21:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -166,7 +166,8 @@ function updateDirectionText(angle) {
 
     }
 }
-const socket = new WebSocket('wss://10.249.1.125:1880/control');
+// const socket = new WebSocket('wss://10.249.1.125:1880/control');
+const socket = new WebSocket('wss://192.168.205.242:1880/control');
 // DATA FORMAT TO SEND TO NODE FOR CONTROLLING WHEEL
 const data = {
     speedValue: 0,
