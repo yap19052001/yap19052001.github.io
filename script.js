ed7d1e6b3c6e2f584c20affb6cf1dbfc7863b2bf
yap19052001 <yap19052001@gmail.com>
2025-04-04 17:18:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -166,7 +166,8 @@ function updateDirectionText(angle) {
 
     }
 }
-const socket = new WebSocket('wss://92ac-2401-4900-3dcb-52c4-5ecb-b559-17ec-5f5b.ngrok-free.app/control');
+
+const socket = new WebSocket('wss://4ec3-123-63-218-66.ngrok-free.app/control');
 // const socket = new WebSocket('wss://10.249.1.125:1880/control');
 //const socket = new WebSocket('wss://192.168.205.242:1880/control');
 // DATA FORMAT TO SEND TO NODE FOR CONTROLLING WHEEL
